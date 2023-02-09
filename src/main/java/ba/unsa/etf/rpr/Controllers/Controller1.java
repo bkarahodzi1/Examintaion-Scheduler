09587a82704c47f1ee31f029b72437f7945feaf2
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-09 21:39:47 +0100

    experiments with gui

--- src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.Controllers;
 
 import ba.unsa.etf.rpr.Dao.DaoFactory;
 import ba.unsa.etf.rpr.Exceptions.HospitalException;
+import ba.unsa.etf.rpr.domain.Doctor;
 import ba.unsa.etf.rpr.domain.Patient;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
@@ -20,21 +21,33 @@ public class Controller1 {
     public GridPane WindowId;
 
     public void LogInProcess(ActionEvent actionEvent) {
+        Label lab = new Label();
         try{
-            List<Patient> lista = DaoFactory.PatientDao().getAll();
-            if(lista.size()==0) {
+            List<Doctor> lista = DaoFactory.DoctorDao().getAll();
+            if(UsernameId.getText().trim().isEmpty()) {
+                RowConstraints con = new RowConstraints();
                 UsernameId.setStyle("-fx-border-color: #E92929");
-                Label lab = new Label("Invalid username!");
+                lab.setText("Invalid username!");
                 lab.setStyle("-fx-text-fill: #E92929");
                 WindowId.add(lab, 1, 2);
-                RowConstraints con = new RowConstraints();
                 con.setPrefHeight(USE_COMPUTED_SIZE);
                 WindowId.getRowConstraints().set(2, con);
                 return;
             }
-
         } catch (HospitalException e) {
             throw new RuntimeException(e);
+        }
+        try{
+            Doctor doc = DaoFactory.DoctorDao().getByUsername(UsernameId.getText());
+        } catch (HospitalException e) {
+            RowConstraints con = new RowConstraints();
+            UsernameId.setStyle("-fx-border-color: #E92929");
+            lab.setText("Username doesn't exist!");
+            lab.setStyle("-fx-text-fill: #E92929");
+            WindowId.add(lab, 1, 2);
+            con.setPrefHeight(USE_COMPUTED_SIZE);
+            WindowId.getRowConstraints().set(2, con);
+            return;
         }
     }
 }

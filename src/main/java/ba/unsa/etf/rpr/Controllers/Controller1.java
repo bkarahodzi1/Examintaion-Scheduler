69ed4c30eb7b2c2b1711402e9cb2a470ef25495c
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-09 20:46:06 +0100

    changes

--- src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
@@ -3,7 +3,6 @@ package ba.unsa.etf.rpr.Controllers;
 import ba.unsa.etf.rpr.Dao.DaoFactory;
 import ba.unsa.etf.rpr.Exceptions.HospitalException;
 import ba.unsa.etf.rpr.domain.Patient;
-import com.mysql.cj.result.Row;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -22,14 +21,17 @@ public class Controller1 {
 
     public void LogInProcess(ActionEvent actionEvent) {
         try{
-            List lista = DaoFactory.PatientDao().getAll();
-            UsernameId.setStyle("-fx-border-color: #E92929");
-            Label lab = new Label("Invalid username!");
-            lab.setStyle("-fx-text-fill: #E92929");
-            WindowId.add(lab, 1, 2);
-            RowConstraints con = new RowConstraints();
-            con.setPrefHeight(USE_COMPUTED_SIZE);
-            WindowId.getRowConstraints().set(2, con);
+            List<Patient> lista = DaoFactory.PatientDao().getAll();
+            if(lista.size()==0) {
+                UsernameId.setStyle("-fx-border-color: #E92929");
+                Label lab = new Label("Invalid username!");
+                lab.setStyle("-fx-text-fill: #E92929");
+                WindowId.add(lab, 1, 2);
+                RowConstraints con = new RowConstraints();
+                con.setPrefHeight(USE_COMPUTED_SIZE);
+                WindowId.getRowConstraints().set(2, con);
+                return;
+            }
         } catch (HospitalException e) {
             throw new RuntimeException(e);
         }

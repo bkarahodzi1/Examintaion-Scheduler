cdc0c95b31da5f404f9ce1f40f92acc3df0e3142
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-09 12:08:48 +0100

    FXML field id's

--- src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Controller1.java
@@ -1,4 +1,18 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller1 {
+    public Button LoginId;
+    public Hyperlink CreateAccId;
+    public PasswordField PasswordId;
+    public TextField UsernameId;
+
+    public void LogInProcess(ActionEvent actionEvent) {
+
+    }
 }

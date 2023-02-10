4c9ac6c0d5656d915d90e497438e4be68dc0846f
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-10 19:26:29 +0100

    added specializations #2

--- src/main/java/ba/unsa/etf/rpr/Controllers/SignUp.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/SignUp.java
@@ -1,43 +1,16 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 
 public class SignUp {
 
     public ChoiceBox SpecId;
+    private ObservableList<String> Specs = FXCollections.observableArrayList("Anatomical Pathology", "Anesthesiology", "Cardiology", "Cardiovascular/Thoracic Surgery", "Clinical Immunology/Allergy", "Critical Care Medicine", "Dermatology", "Diagnostic Radiology", "Emergency Medicine", "Endocrinology and Metabolism", "Family Medicine", "Gastroenterology", "General Internal Medicine", "General Surgery", "General/Clinical Pathology", "Geriatric Medicine", "Hematology", "Medical Biochemistry", "Medical Genetics", "Medical Microbiology and Infectious Diseases", "Medical Oncology", "Nephrology", "Neurology", "Neurosurgery", "Nuclear Medicine", "Obstetrics/Gynecology", "Occupational Medicine", "Ophthalmology", "Orthopedic Surgery", "Otolaryngology", "Pediatrics", "Physical Medicine and Rehabilitation (PM & R)", "Plastic Surgery", "Psychiatry", "Public Health and Preventive Medicine (PhPm)", "Radiation Oncology", "Respirology", "Rheumatology", "Urology");
     @FXML
     private void Initialize(){
-        SpecId.getItems().addAll("Anatomical Pathology", "Anesthesiology", "Cardiology", "Cardiovascular/Thoracic Surgery", "Clinical Immunology/Allergy",
-                "Critical Care Medicine",
-                "Dermatology",
-                "Diagnostic Radiology",
-                "Emergency Medicine",
-                "Endocrinology and Metabolism",
-                "Family Medicine",
-                "Gastroenterology",
-                "General Internal Medicine",
-                "General Surgery",
-                "General/Clinical Pathology",
-                "Geriatric Medicine",
-                "Hematology",
-                "Medical Biochemistry",
-                "Medical Genetics",
-                "Medical Microbiology and Infectious Diseases",
-                "Medical Oncology",
-                "Nephrology",
-                "Neurology",
-                "Neurosurgery",
-                "Nuclear Medicine",
-                "Obstetrics/Gynecology",
-                "Occupational Medicine",
-                "Ophthalmology",
-                "Orthopedic Surgery",
-                "Otolaryngology",
-                "Pediatrics",
-                "Physical Medicine and Rehabilitation (PM & R)",
-                "Plastic Surgery",
-                "Psychiatry",
-                "Public Health and Preventive Medicine (PhPm)", "Radiation Oncology", "Respirology", "Rheumatology", "Urology");
+        SpecId.setItems(Specs);
     }
 }

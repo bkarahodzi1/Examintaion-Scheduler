bf88d18be8ccfaf22c7f191a1814c94f74b2d114
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-10 18:27:18 +0100

    changes to controller #8

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -17,10 +18,10 @@ public class App extends Application
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LogIn.fxml"));
-        primaryStage.setTitle("Hospital");
+        primaryStage.setTitle("Hospital log in");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(true);
-        /*primaryStage.getIcons().add(new Image(""));*/
+        primaryStage.getIcons().add(new Image("C:\\Users\\Svage\\IdeaProjects\\projekatB\\src\\main\\resources\\Images\\v987-18a.jpg"));
         primaryStage.show();
     }
     public static void main(String[] args) {

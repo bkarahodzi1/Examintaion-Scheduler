a5859f552015e78609fc72e65a5a1f2718936751
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-11 18:08:40 +0100

    signup changes to if's #3

--- src/main/java/ba/unsa/etf/rpr/Controllers/SignUp.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/SignUp.java
@@ -36,7 +36,7 @@ public class SignUp implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        SpecId.setValue("--Select value--");
+        SpecId.setValue("--Select specialization--");
         SpecId.getItems().setAll("Anatomical Pathology", "Anesthesiology", "Cardiology", "Cardiovascular/Thoracic Surgery", "Clinical Immunology/Allergy", "Critical Care Medicine", "Dermatology", "Diagnostic Radiology", "Emergency Medicine", "Endocrinology and Metabolism", "Family Medicine", "Gastroenterology", "General Internal Medicine", "General Surgery", "General/Clinical Pathology", "Geriatric Medicine", "Hematology", "Medical Biochemistry", "Medical Genetics", "Medical Microbiology and Infectious Diseases", "Medical Oncology", "Nephrology", "Neurology", "Neurosurgery", "Nuclear Medicine", "Obstetrics/Gynecology", "Occupational Medicine", "Ophthalmology", "Orthopedic Surgery", "Otolaryngology", "Pediatrics", "Physical Medicine and Rehabilitation (PM & R)", "Plastic Surgery", "Psychiatry", "Public Health and Preventive Medicine (PhPm)", "Radiation Oncology", "Respirology", "Rheumatology", "Urology");
         SpinnerValueFactory<Integer> spin = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,45,0);
         this.SeniorityId.setValueFactory(spin);
@@ -45,22 +45,42 @@ public class SignUp implements Initializable {
 
     public void CreateClicked(ActionEvent actionEvent) throws HospitalException {
         if(NameId.getText().trim().isEmpty()){
+            label5.setPrefHeight(0);
+            label4.setPrefHeight(0);
+            label3.setPrefHeight(0);
+            label2.setPrefHeight(0);
             label1.setPrefHeight(USE_COMPUTED_SIZE);
             return;
         }
-        if(SpecId.getSelectionModel().getSelectedItem().isEmpty()) {
+        if(SpecId.getSelectionModel().getSelectedItem().equals("--Select specialization--")) {
+            label1.setPrefHeight(0);
             label2.setPrefHeight(USE_COMPUTED_SIZE);
+            label3.setPrefHeight(0);
+            label4.setPrefHeight(0);
+            label5.setPrefHeight(0);
             return;
         }
         if(UsernameId.getText().isEmpty()) {
+            label4.setPrefHeight(0);
+            label5.setPrefHeight(0);
             label3.setPrefHeight(USE_COMPUTED_SIZE);
+            label1.setPrefHeight(0);
+            label2.setPrefHeight(0);
             return;
         }
         if(PasswordId.getText().isEmpty()) {
             label4.setPrefHeight(USE_COMPUTED_SIZE);
+            label5.setPrefHeight(0);
+            label1.setPrefHeight(0);
+            label2.setPrefHeight(0);
+            label3.setPrefHeight(0);
             return;
         }
         if(!RepeatId.getText().equals(PasswordId.getText())) {
+            label1.setPrefHeight(0);
+            label2.setPrefHeight(0);
+            label3.setPrefHeight(0);
+            label4.setPrefHeight(0);
             label5.setPrefHeight(USE_COMPUTED_SIZE);
             return;
         }

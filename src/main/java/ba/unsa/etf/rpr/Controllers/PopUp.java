ca2cc1f3cff07bce33415416ebea8dda7586e89f
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-11 18:55:56 +0100

    popup controller #2

--- src/main/java/ba/unsa/etf/rpr/Controllers/PopUp.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/PopUp.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.Controllers;
 
-public class PopUp {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PopUp implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }

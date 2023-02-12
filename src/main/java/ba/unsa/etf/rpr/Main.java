66face3afbe02ebaac6239e57def7eece747c9b4
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-12 22:44:35 +0100

    Creating cmd app

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -4,15 +4,35 @@ import ba.unsa.etf.rpr.Dao.DaoFactory;
 import ba.unsa.etf.rpr.Exceptions.HospitalException;
 import ba.unsa.etf.rpr.Domain.Doctor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws HospitalException {
-        Doctor d1 = new Doctor();
-        d1.setName("asdfa");
-        d1.setSpecialization("svasdf");
-        d1.setSeniority(5);
-        d1.setId(0);
-        d1.setUsername("Berin");
-        d1.setPassword("asdfasdfas");
-        DaoFactory.DoctorDao().add(d1);
+        String input;
+        do {
+            String username = new String();
+            System.out.println("Press 1 to log in, 2 to create a new account or 0 to exit. Than press enter.");
+            Scanner in = new Scanner(System.in);
+            input = new String(in.nextLine());
+            if (input.equals("1")) {
+                LogIn();
+                break;
+            } else if (input.equals("2")) {
+                CreateAccount();
+                break;
+            }
+            else if (input.equals("0")){
+                System.out.println("Goodbye.");
+                return;
+            }
+            else System.out.println("Please follow the given instructions.");
+        } while (true);
+    }
+    public static void LogIn(){
+
+    }
+
+    public static void CreateAccount() {
+
     }
 }

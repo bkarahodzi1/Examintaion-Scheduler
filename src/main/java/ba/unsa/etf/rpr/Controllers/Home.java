c8af1bccfd202fc9e5284545c6647f8287f147df
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-12 11:27:18 +0100

    Home.fxml changes #6 Making tableView

--- src/main/java/ba/unsa/etf/rpr/Controllers/Home.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Home.java
@@ -1,11 +1,13 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import ba.unsa.etf.rpr.Domain.Examination;
+import ba.unsa.etf.rpr.Domain.Patient;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class Home {
 
     public TableView tableViewId;
-    public TableColumn patientId;
-    public TableColumn diagnosisId;
+    public TableColumn<Patient, String> patientId;
+    public TableColumn<Examination, String> diagnosisId;
 }

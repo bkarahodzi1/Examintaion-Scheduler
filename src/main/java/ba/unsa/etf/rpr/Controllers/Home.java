c9eb3944eb043b3423ea6f0fc99d5f660db1cb28
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-12 11:22:44 +0100

    Home.fxml changes #5 Home controller

--- src/main/java/ba/unsa/etf/rpr/Controllers/Home.java
+++ src/main/java/ba/unsa/etf/rpr/Controllers/Home.java
@@ -1,5 +1,11 @@
 package ba.unsa.etf.rpr.Controllers;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class Home {
 
+    public TableView tableViewId;
+    public TableColumn patientId;
+    public TableColumn diagnosisId;
 }

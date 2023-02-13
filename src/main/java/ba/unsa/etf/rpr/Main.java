f2db9cb16908b9d6580fba45043e1445c8b82f83
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-13 14:06:10 +0100

    Warning resolving

--- src/main/java/ba/unsa/etf/rpr/Main.java
+++ src/main/java/ba/unsa/etf/rpr/Main.java
@@ -15,23 +15,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws HospitalException, InterruptedException {
         String input;
-        String username = new String();
+        String username = "";
+        label:
         do {
             System.out.println("Press 1 to log in, 2 to create a new account or 0 to exit. Than press enter.");
             Scanner in = new Scanner(System.in);
-            input = new String(in.nextLine());
-            if (input.equals("1")) {
-                if(!LogIn())continue;
-                break;
-            } else if (input.equals("2")) {
-                if(!CreateAccount())continue;
-                break;
+            input = in.nextLine();
+            switch (input) {
+                case "1":
+                    if (!LogIn()) continue;
+                    break label;
+                case "2":
+                    if (!CreateAccount()) continue;
+                    break label;
+                case "0":
+                    System.out.println("Goodbye.");
+                    return;
+                default:
+                    System.out.println("Please follow the given instructions.");
+                    break;
             }
-            else if (input.equals("0")){
-                System.out.println("Goodbye.");
-                return;
-            }
-            else System.out.println("Please follow the given instructions.");
         } while (true);
         Homepage(username);
     }
@@ -39,10 +42,10 @@ public class Main {
 
     private static boolean LogIn(){
         Scanner in = new Scanner(System.in);
-        String password = new String ("");
+        String password = "";
         try {
             System.out.print("Username: ");
-            String username = new String(in.nextLine());
+            String username = in.nextLine();
             System.out.print("Password: ");
             /*Console console = System.console();
             char[] password = console.readPassword();*/
@@ -64,12 +67,12 @@ public class Main {
 
 
     private static boolean CreateAccount() throws HospitalException, InterruptedException {
-        String specialization = new String("");
-        String name = new String("");
-        String seniority = new String("");
-        String username = new String("");
-        String password = new String("");
-        String passrepeat = new String("");
+        String specialization;
+        String name;
+        String seniority;
+        String username;
+        String password;
+        String passrepeat;
         do {
             System.out.println("Full name: ");
             Scanner in = new Scanner(System.in);
@@ -102,7 +105,7 @@ public class Main {
             break;
         }while(true);
         do {
-            List<String> Specializations = new ArrayList<String>(Arrays.asList("Anatomical Pathology", "Anesthesiology", "Cardiology", "Cardiovascular/Thoracic Surgery", "Clinical Immunology/Allergy", "Critical Care Medicine", "Dermatology", "Diagnostic Radiology", "Emergency Medicine", "Endocrinology and Metabolism", "Family Medicine", "Gastroenterology", "General Internal Medicine", "General Surgery", "General/Clinical Pathology", "Geriatric Medicine", "Hematology", "Medical Biochemistry", "Medical Genetics", "Medical Microbiology and Infectious Diseases", "Medical Oncology", "Nephrology", "Neurology", "Neurosurgery", "Nuclear Medicine", "Obstetrics/Gynecology", "Occupational Medicine", "Ophthalmology", "Orthopedic Surgery", "Otolaryngology", "Pediatrics", "Physical Medicine and Rehabilitation (PM & R)", "Plastic Surgery", "Psychiatry", "Public Health and Preventive Medicine (PhPm)", "Radiation Oncology", "Respirology", "Rheumatology", "Urology"));
+            List<String> Specializations = new ArrayList<>(Arrays.asList("Anatomical Pathology", "Anesthesiology", "Cardiology", "Cardiovascular/Thoracic Surgery", "Clinical Immunology/Allergy", "Critical Care Medicine", "Dermatology", "Diagnostic Radiology", "Emergency Medicine", "Endocrinology and Metabolism", "Family Medicine", "Gastroenterology", "General Internal Medicine", "General Surgery", "General/Clinical Pathology", "Geriatric Medicine", "Hematology", "Medical Biochemistry", "Medical Genetics", "Medical Microbiology and Infectious Diseases", "Medical Oncology", "Nephrology", "Neurology", "Neurosurgery", "Nuclear Medicine", "Obstetrics/Gynecology", "Occupational Medicine", "Ophthalmology", "Orthopedic Surgery", "Otolaryngology", "Pediatrics", "Physical Medicine and Rehabilitation (PM & R)", "Plastic Surgery", "Psychiatry", "Public Health and Preventive Medicine (PhPm)", "Radiation Oncology", "Respirology", "Rheumatology", "Urology"));
             System.out.println("Specialization: ");
             Scanner in = new Scanner(System.in);
             specialization = in.nextLine();
@@ -178,7 +181,7 @@ public class Main {
     }
 
 
-    private static void Homepage(String username) throws HospitalException, InterruptedException {
+    private static void Homepage(String username) throws HospitalException {
         Scanner in = new Scanner(System.in);
         System.out.println("\nWelcome!\n");
         List<Patient> patients = DaoFactory.PatientDao().getAll();
@@ -199,21 +202,53 @@ public class Main {
                     }
                 }
             }
+            label:
             do{
                 System.out.println("\nTo search for any patient type 1, to see all patients type 2, to see your patient list again type 3, to exit type 0.");
-                String input = new String("");
+                String input;
                 input = in.nextLine();
-                if (input.equals("0")) {
-                    System.out.println("Goodbye.");
-                    Thread.sleep(1000);
-                    return;
-                }
-                else if(input.equals("1")){
-                    System.out.println("Search: ");
-                    String search = new String(in.nextLine());
-                    System.out.println("Patient             Place               Address             Phone number        Birth date          Health insurance");
-                    for (Patient p : patients) {
-                        if(p.getName().contains(search)){
+                switch (input) {
+                    case "0":
+                        System.out.println("Goodbye.");
+                        return;
+                    case "1":
+                        System.out.println("Search: ");
+                        String search = in.nextLine();
+                        System.out.println("Patient             Place               Address             Phone number        Birth date          Health insurance");
+                        for (Patient p : patients) {
+                            if (p.getName().contains(search)) {
+                                String space = " ";
+                                if (p.getName().length() >= 17) {
+                                    System.out.print(p.getName().substring(0, 17));
+                                    System.out.print("...");
+                                } else System.out.print(p.getName());
+                                System.out.print(space.repeat(20 - p.getName().length()));
+                                if (p.getPlace().length() >= 17) {
+                                    System.out.print(p.getPlace().substring(0, 17));
+                                    System.out.print("...");
+                                } else System.out.print(p.getPlace());
+                                System.out.print(space.repeat(20 - p.getPlace().length()));
+                                if (p.getAddress().length() >= 17) {
+                                    System.out.print(p.getAddress().substring(0, 17));
+                                    System.out.print("...");
+                                } else System.out.print(p.getAddress());
+                                System.out.print(space.repeat(20 - p.getAddress().length()));
+                                System.out.print(p.getPhone_num());
+                                if (p.getPhone_num() != null)
+                                    System.out.print(space.repeat(20 - p.getPhone_num().length()));
+                                else System.out.print(space.repeat(16));
+                                System.out.print(p.getBirth_date());
+                                System.out.print(space.repeat(20 - p.getBirth_date().toString().length()));
+                                System.out.println(p.isHealth_insurance());
+                            }
+                        }
+                        break;
+                    case "3":
+                        break label;
+                    case "2":
+                        System.out.println("All patients: ");
+                        System.out.println("Patient             Place               Address             Phone number        Birth date          Health insurance");
+                        for (Patient p : patients) {
                             String space = " ";
                             if (p.getName().length() >= 17) {
                                 System.out.print(p.getName().substring(0, 17));
@@ -238,37 +273,7 @@ public class Main {
                             System.out.print(space.repeat(20 - p.getBirth_date().toString().length()));
                             System.out.println(p.isHealth_insurance());
                         }
-                    }
-                }
-                else if(input.equals("3"))break;
-                else if(input.equals("2")){
-                    System.out.println("All patients: ");
-                    System.out.println("Patient             Place               Address             Phone number        Birth date          Health insurance");
-                    for (Patient p : patients) {
-                        String space = " ";
-                        if (p.getName().length() >= 17) {
-                            System.out.print(p.getName().substring(0, 17));
-                            System.out.print("...");
-                        } else System.out.print(p.getName());
-                        System.out.print(space.repeat(20 - p.getName().length()));
-                        if (p.getPlace().length() >= 17) {
-                            System.out.print(p.getPlace().substring(0, 17));
-                            System.out.print("...");
-                        } else System.out.print(p.getPlace());
-                        System.out.print(space.repeat(20 - p.getPlace().length()));
-                        if (p.getAddress().length() >= 17) {
-                            System.out.print(p.getAddress().substring(0, 17));
-                            System.out.print("...");
-                        } else System.out.print(p.getAddress());
-                        System.out.print(space.repeat(20 - p.getAddress().length()));
-                        System.out.print(p.getPhone_num());
-                        if (p.getPhone_num() != null)
-                            System.out.print(space.repeat(20 - p.getPhone_num().length()));
-                        else System.out.print(space.repeat(16));
-                        System.out.print(p.getBirth_date());
-                        System.out.print(space.repeat(20 - p.getBirth_date().toString().length()));
-                        System.out.println(p.isHealth_insurance());
-                    }
+                        break;
                 }
             }while(true);
         }while(true);

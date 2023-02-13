b5b4aeed8d5492a2a042b41a11f8723903fc7cbf
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-02-14 00:29:53 +0100

    DataBaseDump and javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ba.unsa.etf.rpr"},{"l":"ba.unsa.etf.rpr.Business"},{"l":"ba.unsa.etf.rpr.Controllers"},{"l":"ba.unsa.etf.rpr.Dao"},{"l":"ba.unsa.etf.rpr.Domain"},{"l":"ba.unsa.etf.rpr.Exceptions"}];updateSearchResults();
